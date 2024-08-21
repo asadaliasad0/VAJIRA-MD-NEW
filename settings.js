3b712355b17924d1358b12df760b61e3378af5b8
asadaliasad0 <usamaijaz2030@gmail.com>
2024-08-21 11:32:52 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '923039774224' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || ''
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=aZt0VAYK#eAv526xvPQTawrnqCoriXN2SS25iWuSCOVs3O3GeCy4'
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
